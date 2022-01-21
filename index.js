6210dbf6bbd9e2bb413b11c9fd1a214bcee481ec
Ryan Souza <ryansateles@gmail.com>
2022-01-21 10:57:08 -0300

    alguns comentarios

--- index.js
+++ index.js
@@ -1,43 +1,53 @@
-const fs = require('fs');
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-const dotenv = require('dotenv');
+/* Requisitando e instanciando 
+as classes necessarias da API Discord.js */
+const { Client, Intents } = require("discord.js");
+const client = new Client({
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
 
+//para navegar entre os arquivos do computador
+const fs = require("fs");
+
+//configurando o arquivo .env
+const dotenv = require("dotenv");
 dotenv.config();
+
+//Armazenando o Token de cliente e definindo o prefixo para comandos
 const token = process.env.DISCORD_TOKEN;
+const { prefix } = require("./config.json");
 
-const { prefix } = require('./config.json');
-
-client.once('ready', () => {
-    console.log(`Estou logado com a tag ${client.user.tag}!`);
+// Quando o cliente estiver pronto, rodar esse código
+client.once("ready", () => {
+	console.log(`Estou logado com a tag ${client.user.tag}!`);
 });
 
 const commandArray = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs
+	.readdirSync("./commands")
+	.filter((file) => file.endsWith(".js"));
 
-for (const file of commandFiles){
-    const command = require(`./commands/${file}`);
-    commandArray.push(command);
-}
+commandFiles.forEach((file) => {
+	const command = require(`./commands/${file}`);
+	commandArray.push(command);
+});
 
-client.on('messageCreate', (message) => {
-    if (!isCommand(message, prefix)) return;
+client.on("messageCreate", (message) => {
+	if (!isCommand(message, prefix)) return;
 
-    const command = getCommand(message, prefix);
+	const command = getCommand(message, prefix);
 
-    commandArray.forEach(comm => comm.name === command ? comm.execute(message) : null);
-
+	commandArray.forEach((comm) =>
+		comm.name === command ? comm.execute(message) : null
+	);
 });
 
 function isCommand(message, prefix) {
-    if (message.content.startsWith(prefix)) {
-        return true;
-    }
-    return false;
+	return message.content.startsWith(prefix) ? true : false;
 }
 
 function getCommand(message, prefix) {
-    return message.content.replace(prefix, '');
+	return message.content.replace(prefix, "");
 }
 
+//Loga no discord usando o Token de cliente
 client.login(token);
